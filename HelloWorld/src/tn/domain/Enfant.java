7b0486a68ec6c39e2351c50149944f5eab9dc516
Ahmed Mehdi Laabidi <ahmed.mehdi.laabidi@gmail.com>
2016-01-01 20:25:34 +0100

    add two classes Enfant and Personne Signed-off-by: Ahmed Mehdi Laabidi <ahmed.mehdi.laabidi@gmail.com>

---
+++ HelloWorld/src/tn/domain/Enfant.java
@@ -0,0 +1,15 @@
+package tn.domain;
+
+public class Enfant {
+	
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
