f943b5e503063f94ec0f10664848202049b23968
Ahmed Mehdi Laabidi <ahmed.mehdi.laabidi@gmail.com>
2016-01-01 20:25:34 +0100

    add class Utils
    Signed-off-by: Ahmed Mehdi Laabidi <ahmed.mehdi.laabidi@gmail.com>

---
+++ HelloWorld/src/tn/utils/Utils.java
@@ -0,0 +1,15 @@
+package tn.utils;
+
+public class Utils {
+	
+	private String dbUtils;
+
+	public String getDbUtils() {
+		return dbUtils;
+	}
+
+	public void setDbUtils(String dbUtils) {
+		this.dbUtils = dbUtils;
+	}
+
+}
